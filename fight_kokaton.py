dc5e5cb13ac2a100cbba695e104f14f75411882d
c0a25012e8 <c0a25012e8@edu.teu.ac.jp>
2026-06-23 17:59:48 +0900

    クラスを定義するときは改行が欲しい#1

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -114,6 +114,7 @@ class Beam:
         if check_bound(self.rct) == (True, True):
             self.rct.move_ip(self.vx, self.vy)
             screen.blit(self.img, self.rct)
+            
 class Bomb:
     """
     爆弾に関するクラス
